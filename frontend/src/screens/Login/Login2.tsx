1fbea559be8cf3ba0b0b235944849e98e812428a
Nakul Prasad <nakulprasad10@outlook.com>
2024-03-06 12:01:18 +0530

    minor change

--- frontend/src/screens/Login/Login2.tsx
+++ frontend/src/screens/Login/Login2.tsx
@@ -1,15 +1,20 @@
-import React from "react";
-import emailIcon from "../../assets/email.png";
-import passwordIcon from "../../assets/password.png";
+import React, { useState } from "react";// @ts-ignore
+import emailIcon from "../../assets/email.png"; // @ts-ignore
+import passwordIcon from "../../assets/password.png";// @ts-ignore
 import personIcon from "../../assets/person.png";
 import "./Login2.css";
 
 const Login2 = () => {
+  const[action, setAction] = useState("Sign Up");
+  const handleOnClick = (action : string) => {
+    setAction(action);
+ };
+
   return (
     <section className="container flexbox" id="login">
       <div className="form-container">
         <div className="header flexbox">
-          <div className="text">Sign Up</div>
+          <div className="text">{action}</div>
           <div className="underline"></div>
         </div>
         <div className="inputs">
@@ -32,8 +37,8 @@ const Login2 = () => {
         Lost Password ? <span> Click Here </span>
       </div>
       <div className="submit-container flexbox">
-        <button className="submit">Sign Up</button>
-        <button className="submit">Login</button>
+        <button className="submit" onClick={() => handleOnClick("Sign Up")}>{action?"Sign Up" : "Login"}</button>
+        <button className="submit" onClick={() => handleOnClick("Login")}>{action?"Login" : "Sign Up"}</button>
       </div>
     </section>
   );
