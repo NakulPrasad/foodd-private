96642d461ae0fc58e9b64a8b65a174781948c73e
Nakul Prasad <nakulprasad10@outlook.com>
2024-06-02 17:18:41 +0530

    minor change

--- frontend/src/configs/URLs.js
+++ frontend/src/configs/URLs.js
@@ -1,11 +1,12 @@
-const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:80/api" : `${process.env.VITE_APP_BASE_URL}/api`
+const NEW_URL = `${process.env.BASE_URL || 'https://foodd-mern.vercel.app'}/api`;
+const BASE_URL = process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/api"
+    : NEW_URL;
 
 export const URLs = {
-    
-    getFoodData : `${BASE_URL}/home/getFoodData`,
-    loginUser :  `${BASE_URL}/user/login`,
-    postOrder : `${BASE_URL}/order/orderCheckout`,
-    getOrders : `${BASE_URL}/order/getMyOrders`,
-    addUser : `${BASE_URL}/user/addUser`,
-
-}
+    getFoodData: `${BASE_URL}/home/getFoodData`,
+    loginUser: `${BASE_URL}/user/login`,
+    postOrder: `${BASE_URL}/order/orderCheckout`,
+    getOrders: `${BASE_URL}/order/getMyOrders`,
+    addUser: `${BASE_URL}/user/addUser`,
+};
