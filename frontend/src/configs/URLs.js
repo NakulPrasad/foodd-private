cdf8e41aa6ab0c0863c4e791a7e06e5dae3cb7a0
Nakul Prasad <nakulprasad10@outlook.com>
2024-06-02 15:42:53 +0530

    minor changes

--- frontend/src/configs/URLs.js
+++ frontend/src/configs/URLs.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:80/api" : "/api/"
+const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:80/api" : `${process.env.VITE_APP_BASE_URL}/api`
 
 export const URLs = {
     
--- index.js
+++ index.js
@@ -21,21 +21,25 @@ app.use(morgan("dev"));
 
 connectToDB()
 
+app.use('/api',apiRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(join(__dirname, './client/build')));
+  app.use(express.static(process.cwd() + '/frontend/dist'));
 
   app.get('*', function (req, res) {
-    res.sendFile(join(__dirname, "./client/build/index.html"), function (err) {
-      res.status(500).send(err);
-    })
-  })
+    res.sendFile(process.cwd() + "/frontend/dist/index.html", function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
+  });
 }
+
 
 app.get('/', (req, res) => {
   res.send('BACKEND WORKING FINE')
 });
-app.use('/api',apiRouter);
+
 
 app.listen(PORT,()=>{
   console.log(`Server is running at ${PORT}`)
